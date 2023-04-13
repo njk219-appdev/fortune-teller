cbde01afc940aa700a4f052f737fac4e519ec518
Noah Katz <noahkatz@chicagobooth.edu>
2023-04-13 14:38:18 +0000

    All but one

--- config/routes.rb
+++ config/routes.rb
@@ -26,10 +26,10 @@ Rails.application.routes.draw do
   
    get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
    get("/zodiacs/libra", { :controller => "air", :action => "scales" })
-  # get ("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
+   get("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
   
-  # get("/zodiacs/cancer " { :controller => "earth" :action => "crab" })
-  # get("/zodiac/ scorpio", { :controller => "Water", :action => "scorpion" })
+   get("/zodiacs/cancer", { :controller => "water", :action => "crab" })
+   get("/zodiacs/scorpio", { :controller => "water", :action => "scorpion" })
   # get("/zodiacs/pisces", { :controller => "water", :action => "fish " })
 
   # PART 3: MORE R→C→A→V PRACTICE
