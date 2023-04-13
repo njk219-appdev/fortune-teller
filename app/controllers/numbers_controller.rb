8d8d427c04e7c77a22705d76c07b729a5df4f5ba
Noah Katz <noahkatz@chicagobooth.edu>
2023-04-13 13:55:09 +0000

    Unlucky

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,15 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+  def losers
+    @lion = Array.new
+
+    5.times do
+      gazelle = rand(1...100)
+      
+      @lion.push(gazelle)
+    end
+
+    render({ :template => "lottery_stuff/boo.html.erb"})
+  end 
 end
